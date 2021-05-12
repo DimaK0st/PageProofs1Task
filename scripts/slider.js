9cdeff67b6a99ba32114d2ccc87709bb938e180e
DimaK0st <70113281+DimaK0st@users.noreply.github.com>
2021-05-12 23:24:42 +0300

    Add Main Slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
+    $('.owl-carousel1').owlCarousel({
+        items: 1,
+        autoPlay: 30000,
+        itemsDesktop: [1199, 1],
+        itemsDesktopSmall: [800, 1],
+        navigation: true,
+        navigationText: [
+            "<i class='fa fa-chevron-left'></i>",
+            "<i class='fa fa-chevron-right'></i>"
+        ]
+    });
+});
+$(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         items: 4,
         autoPlay: 3000,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
+        itemsDesktop : true,
+        itemsDesktopSmall : true,
+        itemsTablet: true,
+        itemsMobile : true,
         navigation: true,
         navigationText: [
             "<i class='fa fa-chevron-left'></i>",
